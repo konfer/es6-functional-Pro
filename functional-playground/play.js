d0e37eeb06bbdaaca4d9d36597f736dd9547c887
konfer <ly_kof@sina.com>
2018-04-05 01:51:01 +0800

    just some code apply for forEach

--- functional-playground/play.js
+++ functional-playground/play.js
@@ -1,4 +1,30 @@
 import forEach from '../lib/es6-functional.js'
 
-var array = [1,2,3]
+var array = [1,2,3,4,5,6,7]
 forEach(array,(data) => console.log(data))
+
+const unlessFn=(predicate,fn)=>{
+	if(!predicate)
+		fn()
+}
+
+var logFn=(number)=>{
+	console.log(number," is even")
+}
+
+
+//forEach(array,
+//	    (number)=>{
+//			unlessFn((number%2),logFn(number))
+//		}
+//)
+
+
+forEach(array,
+		(number)=>{
+			unlessFn((number%2),
+				     ()=>{
+						console.log(number," is even")
+	               	 }
+       	)
+)
